a60775636d452ba04b31e52279da486ee37d6c5e
tdoshi123 <168225392+tdoshi123@users.noreply.github.com>
2024-09-11 14:41:54 -0400

    Update Lab4_TD.cpp

--- Lab4_TD.cpp
+++ Lab4_TD.cpp
@@ -1,20 +1,118 @@
-// Lab4_TD.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lab4_TD.cpp : This file contains the 'main' function. Program execution begins and ends there. //
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+// Print out the menu of choices for the user to select from
+void printMenu() {
+    cout << "Please Select which operation to perform:" << endl;
+    cout << "\t1. Factorial" << endl;
+    cout << "\t2. Arithmetic Series" << endl;
+    cout << "\t3. Geometric Series" << endl;
+    cout << "\t4. Exit" << endl;
+    cout << "Your Selection: ";
 }
+void factorial() {
+    float x;
+    int i;
+    int a = 1;
+    cout << "Factorial: ";
+    cout << "\nEnter a number: ";
+    cin >> x;
+    while (x < 0){
+        cout << "Nice try, please enter a POSITIVE number...: ";
+        cin >> x;
+        if (x > 0) {
+            break;
+        }
+    }
+            cout << x << "! = ";
+            for (int i = 1; i <= x; i++) {
+                a = a * i;
+                cout << i << " ";
+                if (i == x){
+                    break;
+                }
+                cout << " * ";
+            }
+            cout << " = " << a << endl;
+}
+void arithmetic() {
+    int x;
+    int y;
+    int n;
+    int c;
+    int t;
+    cout << "Arithmetic Series: ";
+    cout << "\nEnter a number to start at: ";
+    cin >> x;
+    cout << "Enter a number to add each time: ";
+    cin >> y;
+    cout << "Enter the number of elements in the series: ";
+    cin >> n;
+    for (int c = 1; c <= n; c++){
+        cout << x << " ";
+        t = t + x;
+        x = x + y;
+        if (c == n){
+            break;
+        }
+        else{
+            cout << "+ ";
+        }
+    }
+    cout << "= " << t << endl;
+}
+void geometric() {
+    int r;
+    int a;
+    int n;
+    int c;
+    int t;
+    cout << "Geometric Series: ";
+    cout << "\nEnter a number to start at: ";
+    cin >> r;
+    cout << "Enter a number to multiply by each time: ";
+    cin >> a;
+    cout << "Enter the number of elements in the series: ";
+    cin >> n;
+    for (int c = 1; c <= n; c++){
+        cout << r << " ";
+        t = t + r;
+        r = r * a;
+        if (c == n){
+            break;
+        }
+        cout << "+ ";
+    }
+    cout << "= " << t;
+}
+int main(){
+    int choice;
+    char again;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    do {
+        printMenu();
+        cin >> choice;
+        
+        // Quit if user chooses to exit (or any invalid choice)
+        if (choice > 3 || choice < 1) {
+            return 0;
+        }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+        else if (choice == 1) {
+            factorial();
+        }
+        else if (choice == 2) {
+            arithmetic();
+        }
+        else if (choice == 3) {
+            geometric();
+        }
+        
+        cout << "\nGo Again? [Y/N] ";
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y');
+}
